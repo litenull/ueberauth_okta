3131ff1b8d0bc4eff04a541cf5ed06b8666814d4
litenull <litenull@users.noreply.github.com>
2024-08-05 13:24:01 +0200

    add state_param_cookie_same_site

--- lib/ueberauth/strategy/okta.ex
+++ lib/ueberauth/strategy/okta.ex
@@ -35,7 +35,8 @@ defmodule Ueberauth.Strategy.Okta do
     authorize_url: "/oauth2/v1/authorize",
     token_url: "/oauth2/v1/token",
     userinfo_url: "/oauth2/v1/userinfo",
-    ignores_csrf_attack: true
+    state_param_cookie_same_site: "None",
+    # ignores_csrf_attack: true
 
   alias Ueberauth.Auth.Info
   alias Ueberauth.Auth.Credentials
